89031c75836209b161e94b6e22a0213452c1e78b
Pedro Saravia <pedro.saravia.q@uni.pe>
2024-09-10 11:53:06 -0500

    add backend homework_2

---
+++ backend/homework_2/method_1.py
@@ -0,0 +1,27 @@
+import timeit
+
+original_list = list(range(1000000))
+copy_timings = []
+
+for i in range(10):
+
+    #! ------------ START ---------------
+    start_copy = timeit.default_timer()
+    copy_list = original_list.copy()        #* list.copy()
+    end_copy = timeit.default_timer()
+    #! ------------- END ----------------
+
+    #* Storing time
+    copy_time = end_copy - start_copy
+    copy_timings.append(copy_time)
+
+
+#* -------- Printing Times --------
+print("\n---------------------------                  list.copy()                 --------------------------------")
+print("\n     " + "     |     ".join(str(t+1) for t in range(10)))
+print("-"*120)
+print("  " + "  |  ".join(f"{t:.5f}" for t in copy_timings))
+
+#* -------- Average Time --------
+avg_execution = sum(copy_timings) / len(copy_timings)
+print(f"\nAverage time: {avg_execution:.5f}")
---
+++ backend/homework_2/script.sh
@@ -0,0 +1,3 @@
+python method_1.py &
+python method_2.py &
+python method_3.py
