c15b21b03cc2d21a9c13912be5c017d2f8c13cdf
Pedro Saravia <pedro.saravia.q@uni.pe>
2024-09-18 13:36:24 -0500

    add devops api-demo

---
+++ devops/api_demo/src/test/java/com/jalasoft/api_demo/ApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalasoft.api_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
